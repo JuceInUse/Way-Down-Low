891c08ee1b253d8dc1ccfc2137a2671d4d0aa72a
JuceInUse <seth.weiand@gmail.com>
2024-10-23 16:54:31 -0500

    Update MOD_ID `way-down-low` => `way_down_low`

--- src/main/java/net/juceinuse/way_down_low/WayDownLow.java
+++ src/main/java/net/juceinuse/way_down_low/WayDownLow.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WayDownLow implements ModInitializer {
-	public static final String MOD_ID = "way-down-low";
+	public static final String MOD_ID = "way_down_low";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override

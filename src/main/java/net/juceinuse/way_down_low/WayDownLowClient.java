ae5db0e47edbf7fea5f75a7d8eb70ed3b9f0da7b
JuceInUse <seth.weiand@gmail.com>
2024-10-23 16:43:11 -0500

    Initial Commit

---
+++ src/main/java/net/juceinuse/way_down_low/WayDownLowClient.java
@@ -0,0 +1,10 @@
+package net.juceinuse.way_down_low;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WayDownLowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/juceinuse/way_down_low/WayDownLowDataGenerator.java
@@ -0,0 +1,11 @@
+package net.juceinuse.way_down_low;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WayDownLowDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
